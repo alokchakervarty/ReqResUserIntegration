c1bc77efff108afcfc0017de033dfcecf2825429
Alok Chakervarty <Alok.Chakervarty@smawins.com>
2025-06-20 13:57:54 +0530

    Fixed error with moq

--- ReqResUserApi.Test/UnitTest1.cs
+++ ReqResUserApi.Test/UnitTest1.cs
@@ -1,4 +1,4 @@
-using AutoMapper;
+
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Options;
 using Moq;
@@ -6,47 +6,35 @@ using ReqResUserApi.Clients;
 using ReqResUserApi.Configuration;
 using ReqResUserApi.Models;
 using ReqResUserApi.Services;
-using System.Threading.Tasks;
-using Xunit;
 namespace ReqResUserApi.Tests
 {
     public class ExternalUserServiceTests
     {
         private readonly Mock<IReqResApiClient> _mockApiClient;
-        private readonly Mock<IMemoryCache> _mockMapper;
+        private readonly IMemoryCache _memoryCache;
         private readonly ExternalUserService _service;
         public ExternalUserServiceTests()
         {
             _mockApiClient = new Mock<IReqResApiClient>();
-            _mockMapper = new Mock<IMemoryCache>();
+            _memoryCache = new MemoryCache(new MemoryCacheOptions());
             var config = Options.Create(new ApiConfiguration
             {
-                
                 BaseUrl = "https://reqres.in/api/",
-                UserCacheExpirationMinutes= 5
+                UserCacheExpirationMinutes = 5
             });
-            _service = new ExternalUserService(_mockApiClient.Object, _mockMapper.Object,config);
+            _service = new ExternalUserService(_mockApiClient.Object, _memoryCache, config);
         }
         [Fact]
         public async Task GetUserByIdAsync_ReturnsMappedUser()
         {
-            // Arrange
-            var mockClient = new Mock<IReqResApiClient>();
-            var memoryCache = new MemoryCache(new MemoryCacheOptions());
+            // Arrange  
             var userDto = new UserDto { id = 2, email = "janet@example.com", first_name = "Janet", last_name = "Weaver" };
-            mockClient.Setup(c => c.GetUserByIdAsync(2)).ReturnsAsync(userDto);
-            var config = Options.Create(new ApiConfiguration
-            {
+            _mockApiClient.Setup(c => c.GetUserByIdAsync(2)).ReturnsAsync(userDto);
 
-                BaseUrl = "https://reqres.in/api/",
-                UserCacheExpirationMinutes = 5
-            });
-            var service = new ExternalUserService(mockClient.Object, memoryCache,config);
+            // Act  
+            var user = await _service.GetUserByIdAsync(2);
 
-            // Act
-            var user = await service.GetUserByIdAsync(2);
-
-            // Assert
+            // Assert  
             Assert.NotNull(user);
             Assert.Equal("Janet Weaver", user.FullName);
             Assert.Equal("janet@example.com", user.Email);
@@ -56,10 +44,7 @@ namespace ReqResUserApi.Tests
         {
             var userId = 2;
             _mockApiClient.Setup(c => c.GetUserByIdAsync(userId)).ReturnsAsync((UserDto)null);
-
             await Assert.ThrowsAsync<NullReferenceException>(() => _service.GetUserByIdAsync(userId));
         }
-        
-        
     }
 }
